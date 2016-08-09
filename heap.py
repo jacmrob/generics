0bf85168cab5599aff3bcadabf6520a6aff9630a
admin <jacqueline.roberti93@gmail.com>
2016-08-09 00:16:03 -0400

    more bugs in the heap

--- heap.py
+++ heap.py
@@ -7,23 +7,30 @@ class HeapNode:
 
 
 class Heap:
+	# binary heap 
+
 	def __init__(self):
-		self.elements = [None]
-		self.length = 0
-		self.complete = False 
+		self.elements = []
+		self.length = -1
 
 	def is_empty(self):
 		return self.length == 0 
 
 	def upheap(self, i):
-		parent = i / 2 if self.is_even(i) else (i - 1) / 2
-		while i > 1 and self.elements[i].priority < self.elements[parent].priority:
-			self.fast_swap(self.elements, i, parent)
-			i = parent
-			parent = i / 2 if self.is_even(i) else (i - 1) / 2 
+		# left children are always odd, right children are always even, root is 0 
+		# thus parent of left child = i - 1 / 2, 
+		# parent of right child = i - 2 / 2
+		if i > 1:
+			parent = (i - 2) / 2 if self.is_even(i) else (i - 1) / 2
+			while i > 1 and self.elements[i].priority < self.elements[parent].priority:
+				self.fast_swap(self.elements, i, parent)
+				i = parent
+				parent = (i - 2) / 2 if self.is_even(i) else (i - 1) / 2  # get next parent 
 
 	def downheap(self):
 		i = 1 
+		# while we have not reached end of heap and priority of i is greater than either left or right child's
+		# swap with lesser child
 		while (i < self.length - 2) and ((self.elements[i].priority < self.elements[i + 1].priority) or (self.elements[i].priority < self.elements[i + 2].priority)):
 			swap_i = i + 1 if self.elements[i + 1].priority < self.elements[i + 2].priority else i + 2
 			self.fast_swap(self.elements, i, swap_i)
@@ -33,7 +40,6 @@ class Heap:
 		n = HeapNode(priority, data)
 		self.elements.append(n)
 		self.length += 1 
-		self.complete = not self.complete  # flip sign
 		self.upheap(self.length)
 
 	def remove_min(self):
